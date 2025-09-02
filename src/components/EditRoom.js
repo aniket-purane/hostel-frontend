4672476fe08a6e797cc68ba639aba9c9dd38a401
Aniket Purane <aniketpurane741@gmail.com>
2025-09-02 18:29:31 +0530

    Fix index.html for Netlify build

--- src/components/EditRoom.js
+++ src/components/EditRoom.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getRoomById, updateRoom } from '../services/RoomService';
-import axios from 'axios';
 
 
 const EditRoom = () => {
